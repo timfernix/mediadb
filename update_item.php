1679403f7efef56be3408e63259888a2312a4dde
Tim <46400905+timfernix@users.noreply.github.com>
2024-11-23 18:34:22 +0100

    ✨ Create update_item.php

---
+++ update_item.php
@@ -0,0 +1,50 @@
+<?php
+$conn = new mysqli("", "", "", "");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$type = $_POST['type'];
+$itemId = $_POST['id'];
+$title = $_POST['title'];
+$originTitle = $_POST['originTitle'];
+$coverUrl = $_POST['coverUrl'];
+$releaseDate = $_POST['releaseDate'];
+$director = $_POST['director'];
+$genre = $_POST['genre'];
+$notes = $_POST['notes'];
+$currentlyWatching = isset($_POST["currentlyWatching"]) ? 1 : 0;
+$length = $_POST['length'];
+$favorite = isset($_POST["favorite"]) ? 1 : 0;
+$watchlist = isset($_POST["watchlist"]) ? 1 : 0;
+
+$sql = "UPDATE mediadb SET 
+        type = ?,
+        title = ?,
+        originTitle = ?,
+        coverUrl = ?, 
+        releaseDate = ?, 
+        director = ?, 
+        genre = ?, 
+        notes = ?,
+        currentlyWatching = ?,
+        length = ?,
+        favorite = ?,
+        watchlist = ?
+        WHERE id = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssssssssssi", $type, $title, $originTitle, $coverUrl, $releaseDate, $director, $genre, $notes, $currentlyWatching, $length, $favorite, $watchlist, $itemId);
+
+if ($stmt->execute() === TRUE) {
+    echo "Item updated successfully!";
+    echo "<script>setTimeout(function(){ window.close(); }, 1000);</script>"; 
+} else {
+    echo "Error updating item: " . $conn->error;
+    echo "<script>setTimeout(function(){ window.close(); }, 1000);</script>"; 
+}
+
+$stmt->close();
+$conn->close();
+?>
