09b578f9398b81ca352f97e248f1765c0fd13dbe
Tim <46400905+timfernix@users.noreply.github.com>
2024-12-27 02:22:52 +0100

    Update load_media_items.php

--- load_media_items.php
+++ load_media_items.php
@@ -1,15 +1,27 @@
 <?php
-$conn = new mysqli("", "", "", "");
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
+$port = ; 
+
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->ssl_set(NULL, NULL, 'ca.pem', NULL, NULL);
+
+if (!$conn->real_connect($host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("SSL connection failed: " . mysqli_connect_error());
 }
 
 $sql = "SELECT * FROM mediadb";
 $result = $conn->query($sql);
 
 $data = array();
-while($row = $result->fetch_assoc()) {
+while ($row = $result->fetch_assoc()) {
     $data[] = $row;
 }
 
