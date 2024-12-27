7f8c643b35f25e56f23e6b93858ba4258ce4afc4
Tim <46400905+timfernix@users.noreply.github.com>
2024-12-27 02:24:40 +0100

    Update update_item.php

--- update_item.php
+++ update_item.php
@@ -1,8 +1,20 @@
 <?php
-$conn = new mysqli("", "", "", "");
+$host = ""; 
+$username = ""; 
+$password = ""; 
+$dbname = "";
+$port = ; 
+
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->ssl_set(NULL, NULL, 'ca.pem', NULL, NULL);
+
+if (!$conn->real_connect($host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("SSL connection failed: " . mysqli_connect_error());
 }
 
 $type = $_POST['type'];
