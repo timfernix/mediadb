3f5ba1828877a58b56a4504905c1bc0052dd88b4
Tim <46400905+timfernix@users.noreply.github.com>
2025-01-22 17:50:19 +0100

    ✨ Updated search_media_items.php

--- search_media_items.php
+++ search_media_items.php
@@ -5,36 +5,38 @@ $password = "";
 $dbname = "";
 $port = ; 
 
-$conn = new mysqli($host, $username, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+function connectToDatabase($host, $username, $password, $dbname, $port) {
+    $conn = new mysqli($host, $username, $password, $dbname, $port);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    if (!$conn->ssl_set(NULL, NULL, 'ca.pem', NULL, NULL)) {
+        die("Failed to set SSL: " . $conn->error);
+    }
+    if (!$conn->real_connect($host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+        die("SSL connection failed: " . mysqli_connect_error());
+    }
+    return $conn;
 }
 
-$conn->ssl_set(NULL, NULL, 'ca.pem', NULL, NULL);
-
-if (!$conn->real_connect($host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("SSL connection failed: " . mysqli_connect_error());
+function searchMediaItems($conn, $query) {
+    $sql = "SELECT * FROM mediadb WHERE title LIKE ? OR originTitle LIKE ? OR genre LIKE ? OR director LIKE ?";
+    $stmt = $conn->prepare($sql);
+    $likeQuery = '%' . $query . '%';
+    $stmt->bind_param("ssss", $likeQuery, $likeQuery, $likeQuery, $likeQuery);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    $stmt->close();
+    return $data;
 }
 
 $query = isset($_GET['query']) ? $_GET['query'] : '';
-
-$sql = "SELECT * FROM mediadb WHERE title LIKE ? OR originTitle LIKE ? OR genre LIKE ? OR director LIKE ?";
-$stmt = $conn->prepare($sql);
-
-$likeQuery = '%' . $query . '%';
-$stmt->bind_param("ssss", $likeQuery, $likeQuery, $likeQuery, $likeQuery);
-
-$stmt->execute();
-$result = $stmt->get_result();
-
-$data = array();
-while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
-}
-
+$conn = connectToDatabase($host, $username, $password, $dbname, $port);
+$data = searchMediaItems($conn, $query);
 echo json_encode($data);
-
-$stmt->close();
 $conn->close();
 ?>
