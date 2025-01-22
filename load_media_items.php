5cdc6cdf0339b5dd45a736089f0e54c9f7de8828
Tim <46400905+timfernix@users.noreply.github.com>
2025-01-22 17:49:49 +0100

    ✨ Updated load_media_items.php

--- load_media_items.php
+++ load_media_items.php
@@ -5,27 +5,35 @@ $password = "";
 $dbname = "";
 $port = ; 
 
-$conn = new mysqli($host, $username, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+function connectToDatabase($host, $username, $password, $dbname, $port) {
+    $conn = new mysqli($host, $username, $password, $dbname, $port);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    if (!$conn->ssl_set(NULL, NULL, 'ca.pem', NULL, NULL)) {
+        die("Failed to set SSL: " . $conn->error);
+    }
+    if (!$conn->real_connect($host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+        die("SSL connection failed: " . mysqli_connect_error());
+    }
+    return $conn;
 }
 
-$conn->ssl_set(NULL, NULL, 'ca.pem', NULL, NULL);
-
-if (!$conn->real_connect($host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("SSL connection failed: " . mysqli_connect_error());
+function fetchMediaItems($conn) {
+    $sql = "SELECT * FROM mediadb";
+    $result = $conn->query($sql);
+    if (!$result) {
+        die("Query failed: " . $conn->error);
+    }
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    return $data;
 }
 
-$sql = "SELECT * FROM mediadb";
-$result = $conn->query($sql);
-
-$data = array();
-while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
-}
-
+$conn = connectToDatabase($host, $username, $password, $dbname, $port);
+$data = fetchMediaItems($conn);
 echo json_encode($data);
-
 $conn->close();
 ?>
